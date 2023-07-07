b9e7d49a6dacf27cfd99fa195f95dc302358183f
Victor Getsov <viktorgetsov@gmail.com>
2023-07-07 18:34:02 +0300

    rewrite checkCorrectness function

--- checkCorrectness.js
+++ checkCorrectness.js
@@ -1,5 +1,16 @@
 const checkCorrectness = ({ correct, actual }) => {
-    return correct.first === actual.first && correct.second === actual.second
+    const correctObjKeys = Object.keys(correct)
+    const actualObjKeys = Object.keys(actual)
+
+    if (correctObjKeys.length < actualObjKeys.length) {
+        throw new Error('You passed more replies than questions')
+    }
+
+    if (!correctObjKeys.every((key) => actualObjKeys.includes(key))) {
+        throw new Error('You passed a reply to a non-existing question')
+    }
+
+    return correctObjKeys.every((question) => correct[question] === actual[question])
 }
 
 module.exports = { checkCorrectness }
--- checkCorrectness.test.js
+++ checkCorrectness.test.js
@@ -15,6 +15,21 @@ test('checkCorrectness returns true for correct responses', () => {
     expect(isCorrect).toBe(true)
 })
 
+test('checkCorrectness returns true for correct responses', () => {
+    const isCorrect = checkCorrectness({
+        correct: {
+            first: 'first correct response',
+            second: 'second response',
+        },
+        actual: {
+            second: 'second response',
+            first: 'first correct response',
+        },
+    })
+
+    expect(isCorrect).toBe(true)
+})
+
 test('checkCorrectness returns false for incorrect responses', () => {
     const isCorrect = checkCorrectness({
         correct: {
@@ -28,6 +43,81 @@ test('checkCorrectness returns false for incorrect responses', () => {
     })
 
     expect(isCorrect).toBe(false)
+})
+
+test('checkCorrectness returns false for incorrect responses', () => {
+    const isCorrect = checkCorrectness({
+        correct: {
+            first: 'first correct response',
+            second: 'second response',
+        },
+        actual: {
+            first: '',
+            second: 'second response',
+        },
+    })
+
+    expect(isCorrect).toBe(false)
+})
+
+test('checkCorrectness returns false for incorrect responses', () => {
+    const isCorrect = checkCorrectness({
+        correct: {
+            first: 'first correct response',
+            second: 'second response',
+        },
+        actual: {
+            first: 'first actual response',
+            second: '',
+        },
+    })
+
+    expect(isCorrect).toBe(false)
+})
+
+test('checkCorrectness returns false for incorrect responses', () => {
+    expect(() =>
+        checkCorrectness({
+            correct: {
+                first: 'first correct response',
+                second: 'second response',
+            },
+            actual: {
+                first: 'first correct response',
+                third: 'third response',
+            },
+        })
+    ).toThrow()
+})
+
+test('checkCorrectness returns false for incorrect responses', () => {
+    expect(() =>
+        checkCorrectness({
+            correct: {
+                first: 'first correct response',
+                second: 'second response',
+            },
+            actual: {
+                firstsecond: 'first correct response',
+            },
+        })
+    ).toThrow()
+})
+
+test('checkCorrectness returns false for incorrect responses', () => {
+    expect(() =>
+        checkCorrectness({
+            correct: {
+                first: 'first correct response',
+                second: 'second response',
+            },
+            actual: {
+                first: 'first correct response',
+                second: 'second response',
+                third: 'third response',
+            },
+        })
+    ).toThrow()
 })
 
 test('checkCorrectness to throw if wrong value is passed', () => {
